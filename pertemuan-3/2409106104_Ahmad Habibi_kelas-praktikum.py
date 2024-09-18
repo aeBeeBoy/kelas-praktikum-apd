b3b86577898ca71a54504bc314d772a81443d689
aeBeeBoy <habyby1907@gmail.com>
2024-09-18 03:24:22 -0700

    pertemuan-3

---
+++ pertemuan-3/2409106104_Ahmad Habibi_kelas-praktikum.py
@@ -0,0 +1,57 @@
+# cuaca = "mendung"
+# if cuaca == "mendung":
+#     print("jangan keluar rumah")
+# else :
+#     print("silahkan keluar rumah")
+
+
+
+# Umur = int(input("Masukkan umur Anda : "))
+
+# if Umur > 0 :
+#     if Umur <= 10:
+#         print( "Umur Anda termasuk kategori anak-anak")
+#     elif Umur <= 18:
+#         print( "Umur Anda termasuk kategori remaja")
+#     elif Umur <=35:
+#         print("Umur Anda termasuk kategori dewasa")
+#     elif Umur <=65:
+#         print("Umur Anda termasuk kategori paruh baya")
+#     else:
+#         print("Umur Anda termasuk kategori Tua")
+# else:
+#     print ("input usia harus bilangan positif")
+
+
+
+# nim = int(input("masukkan nim :"))
+
+# if nim >= 1 and nim <= 50 :
+#     if nim >=1 and nim <= 25:
+#         print("kelas A1")
+#     else : 
+#         print("kelas A2")
+# elif nim >= 51 and nim <= 100:
+#     if nim >= 51 and nim <= 75:
+#         print("kelas B1")
+#     else :
+#         print("kelas B2")
+# elif nim >= 101 and nim <= 120:
+#     if nim >= 101 and nim <= 110:
+#         print("kelas C1")
+#     else :
+#         print("kelas C2")
+# else :
+#     print("bukan mahasiswa")
+
+
+
+# angka = int(input("Masukkan angka: "))
+# result = "Genap" if angka % 2 == 0 else "Ganjil"
+# print(angka, "adalah angka",result)
+
+
+
+# nim = int(input("Masukkan Nim : "))
+# hasil = "kelas A" if nim >= 1 and nim <= 50 else "kelas B" if nim >=51 and nim <= 100 else "kelas C" if nim >= 101 and nim <= 120 else "mahasiswa siluman"
+# print(hasil)
